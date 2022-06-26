b32c6a4eb6616d00831da1cf500b554f953eeb8f
mayanksingh10 <108238137+mayanksingh10@users.noreply.github.com>
2022-06-27 01:16:59 +0530

    Add files via upload
    importing 2 lines

---
+++ MY FIRST NOTEBOOK.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("hello world")
+
+
+# In[2]:
+
+
+print("hi guys! My name is Mayank. How are you doin?")
+
+
+# In[3]:
+
+
+x=10
+y=5
+print(x)
+
+
+# In[4]:
+
+
+x=10
+y=5
+print("x")
+
+
+# In[5]:
+
+
+print("sum of x and y=",x+y)
+
+
+# In[6]:
+
+
+print(x+y)
+
+
+# In[7]:
+
+
+x=10
+y=15
+c=x*x+2*x*y+y*y
+print("x+y whole square is",c)
+c=x*x-2*x*y+y*y
+print("x-y whole square is",c)
+
+
+# In[8]:
+
+
+print("the value of x and y are",x, "and",y,"respectively")
+
+
+# In[9]:
+
+
+x=10
+y=5
+print(difference of x and y=",x-y")
+
+
+# In[ ]:
+
+
+
+
